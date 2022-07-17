d7b822cc39614eff2a2b3dcffa604b11c3cf94d8
o4codes <oforkansi.shadrach@gmail.com>
2022-07-17 17:01:38 +0100

    added required libraries

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
+require('dotenv').config();
 
 const app = express();
 
+app.use(express.json());
+app.use(morgan('dev'));
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
-    });
+    }
+);
